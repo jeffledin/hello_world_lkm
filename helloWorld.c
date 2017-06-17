45d3584ab0da381c91d7401135e172008f08d073
jeffledin <ledin.jeffrey@gmail.com>
2017-06-17 00:37:05 -0500

    Update helloWorld.c

--- helloWorld.c
+++ helloWorld.c
@@ -1,14 +1,3 @@
-/**
- * @file    hello.c
- * @author  Derek Molloy
- * @date    4 April 2015
- * @version 0.1
- * @brief  An introductory "Hello World!" loadable kernel module (LKM) that can display a message
- * in the /var/log/kern.log file when the module is loaded and removed. The module can accept an
- * argument when it is loaded -- the name, which appears in the kernel log files.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
-*/
-
 #include <linux/init.h>             // Macros used to mark up functions e.g., __init __exit
 #include <linux/module.h>           // Core header for loading LKMs into the kernel
 #include <linux/kernel.h>           // Contains types, macros, functions for the kernel
@@ -18,18 +7,8 @@ MODULE_AUTHOR("Jeff Ledin");      ///< The author -- visible when you use modinf
 MODULE_DESCRIPTION("A simple Linux driver for the RPi.");  ///< The description -- see modinfo
 MODULE_VERSION("0.1");              ///< The version of the module
 
-static char *name = "world";        ///< An example LKM argument -- default value is "world"
-module_param(name, charp, S_IRUGO); ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
-MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parameter description
-
-/** @brief The LKM initialization function
- *  The static keyword restricts the visibility of the function to within this C file. The __init
- *  macro means that for a built-in driver (not a LKM) the function is only used at initialization
- *  time and that it can be discarded and its memory freed up after that point.
- *  @return returns 0 if successful
- */
 static int __init helloRPi_init(void){
-   printk(KERN_INFO "Hello %s from the RPi LKM!\n", name);
+   printk(KERN_INFO "Hello world!\n");
    return 0;
 }
 
@@ -38,7 +17,7 @@ static int __init helloRPi_init(void){
  *  code is used for a built-in driver (not a LKM) that this function is not required.
  */
 static void __exit helloRPi_exit(void){
-   printk(KERN_INFO "Goodbye %s from the RPi LKM!\n", name);
+   printk(KERN_INFO "Goodbye world!");
 }
 
 /** @brief A module must use the module_init() module_exit() macros from linux/init.h, which
